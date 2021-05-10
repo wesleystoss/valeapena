6e75322f5a42ed8ccf60eb4c77c7fd52b09b166e
Wesley Santos <wesley.santos@grancursosonline.com.br>
2021-05-10 10:22:37 -0300

    FIX: Correções

--- templates/static/JS/scripts.js
+++ templates/static/JS/scripts.js
@@ -1,3 +1,14 @@
 function mostrar_busca_avancada(){
     document.getElementById("busca_avancada").style.display = "block"
 }
+function m_usu_news(){
+    var x = document.getElementById("ver_newslestter").value
+
+    if(x === "Ver"){
+         document.getElementById("lista_news").style.display = "none"
+         document.getElementById("ver_newslestter").value = ""
+     }else{
+        document.getElementById("lista_news").style.display = "block"
+        document.getElementById("ver_newslestter").value = "Ver"
+    }
+ }
